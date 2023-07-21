34fc32e611d9740361efe30982d5897f655417ca
Michael Mendelsohn <mmendels909@gmail.com>
2023-07-21 15:23:49 -0700

    add constant functions and queues json, plus more work on working file including epoch second to datetime conversion

---
+++ v1/constants.py
@@ -0,0 +1,17 @@
+import json
+
+def get_items(lol_watcher):
+    latest = lol_watcher.data_dragon.versions_for_region('na1')['n']['champion']
+    static_item_list = lol_watcher.data_dragon.items(latest, False)
+    return {item:static_item_list['data'][item]['name'] for item in static_item_list['data'].keys()}
+   
+def get_spells(lol_watcher):
+    latest = lol_watcher.data_dragon.versions_for_region('na1')['n']['champion']
+    static_spell_list = lol_watcher.data_dragon.summoner_spells(latest, False)
+    return {static_spell_list['data'][spell]['key']:static_spell_list['data'][spell]['name'] for spell  in static_spell_list['data'].keys()}
+
+def get_queues():
+    with open('queues.json') as user_file:
+        queue_json = json.load(user_file)
+    return { queue['queueId'] : queue['description'] for queue in queue_json['queues_list']}
+    
