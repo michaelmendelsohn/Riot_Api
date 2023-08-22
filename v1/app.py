a34745e2d2f088bfbdd67a0b35de792144102eaa
Michael Mendelsohn <mmendels909@gmail.com>
2023-08-21 17:14:26 -0700

    more app stuff. Need to fix empty summoner name

--- v1/app.py
+++ v1/app.py
@@ -20,31 +20,64 @@ with st.container(): # this is optional
 #     with open(filename) as f:
 #         st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 # local_css("../style/style.css")
-def display_stats(summoner_name, role):
+def display_stats(main_summoner_name, role, teammates_dict):
     engine = help.create_mysql_engine()
-    stats_df = af.stats_at_min(summoner_name, role, engine)
+    #stats_df = af.stats_at_min(summoner_name, role, engine)
+    stats_df = af.stats_at_min_with_teammates(main_summoner_name, role, engine, teammates_dict)
     st.write(stats_df)
     return
 
 # ---- What I do
+
 with st.container():
+    
     st.write("---")
     left_col, right_col = st.columns((1,1))
+
+
+    
     with left_col:
-        summoner_name = st.text_input("Enter Summoner Name",key="summoner_name_inpute")
-        role = st.selectbox('Choose your Role.',('ALL ROLES', 'TOP', 'JUNGLE', 'MIDDLE','BOTTOM', 'SUPPORT'))
-        if st.button(f"Pull New Data for {summoner_name}"):
+
+        summ_name_col, role_col = st.columns((1,1))
+        with summ_name_col:
+            summoner_name_0 = st.text_input("Enter Summoner Name", key="summoner_name_input_0")
+            summoner_name_1 = st.text_input("Enter 1st Teammate Name", key="summoner_name_input_1")
+            summoner_name_2 = st.text_input("Enter 2nd Teammate Name", key="summoner_name_input_2")
+            summoner_name_3 = st.text_input("Enter 3rd Teammate Name", key="summoner_name_input_3")
+            summoner_name_4 = st.text_input("Enter 4th Teammate Name", key="summoner_name_input_4")
+        
+        with role_col:
+            role_0 = st.selectbox('Choose your Role.',('ALL ROLES', 'TOP', 'JUNGLE', 'MIDDLE','BOTTOM', 'SUPPORT'))
+            role_1 = st.selectbox("Choose 1st Teammate's Role.",('ALL ROLES', 'TOP', 'JUNGLE', 'MIDDLE','BOTTOM', 'SUPPORT'))
+            role_2 = st.selectbox("Choose 2nd Teammate's Role.",('ALL ROLES', 'TOP', 'JUNGLE', 'MIDDLE','BOTTOM', 'SUPPORT'))
+            role_3 = st.selectbox("Choose 3rd Teammate's Role.",('ALL ROLES', 'TOP', 'JUNGLE', 'MIDDLE','BOTTOM', 'SUPPORT'))
+            role_4 = st.selectbox("Choose 4th Teammate's Role.",('ALL ROLES', 'TOP', 'JUNGLE', 'MIDDLE','BOTTOM', 'SUPPORT'))
+
+        teammates_dict = {summoner_name_0:role_0,
+                          summoner_name_1:role_1,
+                          summoner_name_2:role_2,
+                          summoner_name_3:role_3,
+                          summoner_name_4:role_4}
+        teammates_dict_without_main = teammates_dict.copy()
+        teammates_dict_without_main.pop(summoner_name_0)
+
+        if st.button(f"Pull New Data for {summoner_name_0}"):
             num_threads = 4
-            matches_to_slurp = help.find_to_slurp(summoner_name, lol_watcher, 'lol_match_details', engine, region = 'na1')
+            matches_to_slurp = help.find_to_slurp(summoner_name_0, lol_watcher, 'lol_match_details', engine, region = 'na1')
             st.write(f"Pulling and analyzing data for {min(len(matches_to_slurp),100)} games. {num_threads} Concurrent processes. Estimated runtime is {min(2+len(matches_to_slurp)/2,52)} seconds. Max of 100 games.")
-            help.collect_riot_api_data(summoner_name, lol_watcher, 'lol_match_timeline',
+            help.collect_riot_api_data(summoner_name_0, lol_watcher, 'lol_match_timeline',
                         engine, match_upload_limit=100, num_worker_threads=num_threads)
-            help.collect_riot_api_data(summoner_name, lol_watcher, 'lol_match_details',
+            help.collect_riot_api_data(summoner_name_0, lol_watcher, 'lol_match_details',
                                     engine, match_upload_limit=100, num_worker_threads=num_threads)
             rc.determine_roles(engine)
+    
     with right_col:
         if st.button("Go!"):
-            display_stats(summoner_name, role)
+            teammates_string = " and ".join([f"{name} as {teammates_dict_without_main[name]}" for name in teammates_dict_without_main.keys()])
+            st.write(f"Pulling {summoner_name_0}'s {role_0} games {teammates_string}")
+            st.write(teammates_dict)
+            display_stats(summoner_name_0, role_0, teammates_dict)
+
 # Lower Section Stats
 with st.container():
     st.write("---")
