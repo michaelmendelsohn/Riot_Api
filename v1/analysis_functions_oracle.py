cc434a4864b032bb0079b0b936b731a7787f6a3a
Michael Mendelsohn <mmendels909@gmail.com>
2023-09-08 16:47:00 -0700

    pull from oracle and work on website

--- v1/analysis_functions_oracle.py
+++ v1/analysis_functions_oracle.py
@@ -26,23 +26,23 @@ def prep_comparison_df(summoner_name, db_engine, minutes_to_analyse = [10,14], t
     FROM lol_match_timeline time
     INNER JOIN lol_match_details det on time.matchId=det.matchId and time.participantId=det.participantId
     INNER JOIN lol_match_roles rol on rol.matchId=det.matchId and rol.participantId=det.participantId
-    INNER JOIN (select distinct matchid, teamName, LISTAGG(summonerName, '') as summonerNameList FROM lol_match_details group by 1,2) summoner_basis
+    INNER JOIN (select distinct matchid, teamName, LISTAGG(summonerName, '') as summonerNameList FROM lol_match_details group by matchid, teamName) summoner_basis
                 on det.matchid = summoner_basis.matchid
 
     where rol.validRole = 1 and minute in ({minutes_where_sql}) and lower(summonerNameList) LIKE '%{summoner_name.lower()}%' 
     FETCH NEXT 30 ROWS ONLY
     """ # {teammates_where_sql}
     df = pd.read_sql(query, con=db_engine)
-    df_home = df[df['teamMate']==1]
-    df_away = df[df['teamMate']==0]
+    df_home = df[df['TEAMMATE']==1]
+    df_away = df[df['TEAMMATE']==0]
 
-    df_compare = df_home.merge(df_away, how='inner', left_on = ['matchId','role','minute','queueName','gameCreationDate'],
-                                                    right_on = ['matchId','role','minute','queueName','gameCreationDate'])
+    df_compare = df_home.merge(df_away, how='inner', left_on = ['MATCHID','ROLE','MINUTE','QUEUENAME','GAMECREATIONDATE'],
+                                                    right_on = ['MATCHID','ROLE','MINUTE','QUEUENAME','GAMECREATIONDATE'])
     
-    df_compare['cs_diff'] = df_compare['totalCS_x'] - df_compare['totalCS_y']
-    df_compare['xp_diff'] = df_compare['xp_x'] - df_compare['xp_y']
-    df_compare['gold_diff'] = df_compare['totalGold_x'] - df_compare['totalGold_y']
-    df_compare['dmg_diff'] = df_compare['totalDamageDoneToChampions_x'] - df_compare['totalDamageDoneToChampions_y']
+    df_compare['CS_DIFF'] = df_compare['TOTALCS_x'] - df_compare['TOTALCS_y']
+    df_compare['XP_DIFF'] = df_compare['XP_x'] - df_compare['XP_y']
+    df_compare['GOLD_DIFF'] = df_compare['TOTALGOLD_x'] - df_compare['TOTALGOLD_y']
+    df_compare['DMG_DIFF'] = df_compare['TOTALDAMAGEDONETOCHAMPIONS_x'] - df_compare['TOTALDAMAGEDONETOCHAMPIONS_y']
 
     return df_compare
 
@@ -50,18 +50,18 @@ def stats_at_min(summoner_name, role, db_engine, minutes_to_analyse =[14], teamm
                  beg_timestamp= pd.Timestamp('2018-01-01 15:48:49'), end_timestamp= pd.Timestamp('2025-12-31 15:48:49'), return_type = 'styled' ):
     summoner_name = summoner_name.lower()
     df = prep_comparison_df(summoner_name, db_engine, minutes_to_analyse = minutes_to_analyse, teammates_names = teammates_names)
-    cols=['role','championName_x','championName_y','winFlag_x','minute','cs_diff','xp_diff','gold_diff','dmg_diff']
+    cols=['ROLE','CHAMPIONNAME_x','CHAMPIONNAME_y','WINFLAG_x','MINUTE','CS_DIFF','XP_DIFF','GOLD_DIFF','DMG_DIFF']
     if role == 'ALL ROLES':
-        mid_df_10 = df[(df.summonerName_x==summoner_name) &
-                    (df.gameCreationDate > beg_timestamp) &
-                    (df.gameCreationDate < end_timestamp)][cols]
+        mid_df_10 = df[(df.SUMMONERNAME_x==summoner_name) &
+                    (df.GAMECREATIONDATE > beg_timestamp) &
+                    (df.GAMECREATIONDATE < end_timestamp)][cols]
     else:
-        mid_df_10 = df[(df.summonerName_x==summoner_name) & (df.role==role) &
-                    (df.gameCreationDate > beg_timestamp) &
-                    (df.gameCreationDate < end_timestamp)][cols]
-    agg_types={'championName_y':'count','winFlag_x':'mean', 'cs_diff':'mean','xp_diff':'mean', 'gold_diff':'mean', 'dmg_diff':'mean' }
-    rename_dict = {'championName_y':'Games', 'championName_x':'Champion_Name', 'winFlag_x':'Winrate', 'cs_diff':'CS_Diff@14', 'xp_diff':'XP_Diff@14', 'gold_diff':'Gold_Diff@14', 'dmg_diff':'DMG_Diff@14'}
-    x=mid_df_10.groupby('championName_x', as_index=False,).agg(agg_types).rename(columns=rename_dict)
+        mid_df_10 = df[(df.SUMMONERNAME_x==summoner_name) & (df.ROLE==role) &
+                    (df.GAMECREATIONDATE > beg_timestamp) &
+                    (df.GAMECREATIONDATE < end_timestamp)][cols]
+    agg_types={'CHAMPIONNAME_y':'count','WINFLAG_x':'mean', 'CS_DIFF':'mean','XP_DIFF':'mean', 'GOLD_DIFF':'mean', 'DMG_DIFF':'mean' }
+    rename_dict = {'CHAMPIONNAME_y':'Games', 'CHAMPIONNAME_x':'Champion_Name', 'WINFLAG_x':'Winrate', 'CS_DIFF':'CS_Diff@14', 'XP_DIFF':'XP_Diff@14', 'GOLD_DIFF':'Gold_Diff@14', 'DMG_DIFF':'DMG_Diff@14'}
+    x=mid_df_10.groupby('CHAMPIONNAME_X', as_index=False,).agg(agg_types).rename(columns=rename_dict)
     n_games = x.Games.sum()
     total_row = ['Total', n_games,  x.Games.dot(x.Winrate)/n_games,
                                     x.Games.dot(x['CS_Diff@14'])/n_games,
@@ -86,38 +86,35 @@ def stats_at_min_with_teammates(main_summoner_name, role, db_engine, teammates_d
                  beg_timestamp= pd.Timestamp('2018-01-01 15:48:49'), end_timestamp= pd.Timestamp('2025-12-31 15:48:49'), 
                  agg_type='agg', return_type='styled' ):
     df = prep_comparison_df(main_summoner_name, db_engine, minutes_to_analyse = minutes_to_analyse) 
-    cols=['gameCreationDate', 'role','championName_x','championName_y','winFlag_x','minute','cs_diff','xp_diff','gold_diff','dmg_diff']
+    cols=['GAMECREATIONDATE', 'ROLE','CHAMPIONNAME_x','CHAMPIONNAME_y','WINFLAG_x','MINUTE','CS_DIFF','XP_DIFF','GOLD_DIFF','DMG_DIFF']
+
 
     # Let's say we're looking for 3 summoners. There will be 1 match_id  for each  correct summoner_name / role combo.
     # So then we need to select only the match_ids with 3 instances.
-    match_id_mask = [ m_id for m_id, summ_name, role in zip(df.matchId, df.summonerName_x, df.role) 
+    match_id_mask = [ m_id for m_id, summ_name, role in zip(df.MATCHID, df.SUMMONERNAME_x, df.ROLE) 
                         if summ_name in teammates_dict.keys() and (role == teammates_dict[summ_name] or teammates_dict[summ_name] == 'ALL ROLES') ]
 
     val_counts = Counter(match_id_mask)
     valid_matchlist = [m_id for m_id in val_counts if val_counts[m_id]==len(teammates_dict.keys())]
-    df = df[df['matchId'].isin(valid_matchlist)]
-
-    processed_df = df[(df.summonerName_x == main_summoner_name) &
-                (df.gameCreationDate > beg_timestamp) &
-                (df.gameCreationDate < end_timestamp)][cols]
+    df = df[df['MATCHID'].isin(valid_matchlist)]
 
     if role == 'ALL ROLES':
-        processed_df = df[(df.summonerName_x==main_summoner_name) &
-                    (df.gameCreationDate > beg_timestamp) &
-                    (df.gameCreationDate < end_timestamp)][cols]
+        processed_df = df[(df.SUMMONERNAME_x==main_summoner_name) &
+                    (df.GAMECREATIONDATE > beg_timestamp) &
+                    (df.GAMECREATIONDATE < end_timestamp)][cols]
     else:
         processed_df = df[(df.summonerName_x==main_summoner_name) & (df.role==role) &
-                    (df.gameCreationDate > beg_timestamp) &
-                    (df.gameCreationDate < end_timestamp)][cols]    
+                    (df.GAMECREATIONDATE > beg_timestamp) &
+                    (df.GAMECREATIONDATE < end_timestamp)][cols]    
 
     if agg_type != 'agg':
-        rename_dict_2 = {'championName_y':'Enemy_Champion', 'championName_x':'Champion', 'winFlag_x':'Win'}
-        return processed_df.rename(columns=rename_dict_2).sort_values(by='gameCreationDate', ascending = False).reset_index(drop=True)
+        rename_dict_2 = {'CHAMPIONNAME_y':'Enemy_Champion', 'CHAMPIONNAME_X':'Champion', 'WINFLAG_x':'Win'}
+        return processed_df.rename(columns=rename_dict_2).sort_values(by='GAMECREATIONDATE', ascending = False).reset_index(drop=True)
     else:
-        processed_df.drop(['gameCreationDate'], axis=1, inplace=True)
-        agg_types={'championName_y':'count','winFlag_x':'mean', 'cs_diff':'mean','xp_diff':'mean', 'gold_diff':'mean', 'dmg_diff':'mean' }
-        rename_dict = {'championName_y':'Games', 'championName_x':'Champion_Name', 'winFlag_x':'Winrate', 'cs_diff':'CS_Diff@14', 'xp_diff':'XP_Diff@14', 'gold_diff':'Gold_Diff@14', 'dmg_diff':'DMG_Diff@14'}
-        aggregated_df=processed_df.groupby('championName_x', as_index=False,).agg(agg_types).rename(columns=rename_dict)
+        processed_df.drop(['GAMECREATIONDATE'], axis=1, inplace=True)
+        agg_types={'CHAMPIONNAME_y':'count','WINFLAG_x':'mean', 'CS_DIFF':'mean','XP_DIFF':'mean', 'GOLD_DIFF':'mean', 'DMG_DIFF':'mean' }
+        rename_dict = {'CHAMPIONNAME_y':'Games', 'CHAMPIONNAME_x':'Champion_Name', 'WINFLAG_x':'Winrate', 'CS_DIFF':'CS_Diff@14', 'XP_DIFF':'XP_Diff@14', 'GOLD_DIFF':'Gold_Diff@14', 'DMG_DIFF':'DMG_Diff@14'}
+        aggregated_df=processed_df.groupby('CHAMPIONNAME_x', as_index=False,).agg(agg_types).rename(columns=rename_dict)
         n_games = aggregated_df.Games.sum()
         total_row = ['Total', n_games,  aggregated_df.Games.dot(aggregated_df.Winrate)/n_games,
                                         aggregated_df.Games.dot(aggregated_df['CS_Diff@14'])/n_games,
